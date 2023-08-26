790105f55fd101a7058e713c0dc470b6c7717cc2
sebastiangonzalez <dsgspersonal@gmail.com>
2023-08-25 19:52:02 -0600

    added some more visitors to logical operations less tha, less equal and some more of this type and did a few changes to the structure of the global variables and their use in the visitor object structure yeah

--- YAPLVisitor_edited_base.py
+++ YAPLVisitor_edited_base.py
@@ -8,76 +8,69 @@ from table.function_table import *
 from table.types_table import *
 from errors import semanticError
 
-attributeTable = AttributeTable()
-typesTable = TypesTable()
-classTable = ClassTable()
-functionTable = FunctionTable()
-currentScope = 1 
-currentClass = "Debugg" 
-currentMethod = "Debugg" 
-currentMethodId = 0 
-currentMethod = None
-foundErrors = []
-
 class YaplVisitorEditedBase(YAPLVisitor):
+
+    def __init__(self):
+        super().__init__()
+        self.functionTable = FunctionTable()
+        self.attributeTable = AttributeTable()
+        self.typesTable = TypesTable()
+        self.classTable = ClassTable()
+        self.currentMethod = None
+        self.currentScope = 1
+        self.currentClass = "Debugg"
+        self.currentMethodId = 0
+        self.foundErrors = []
+    
     def visitClass(self, ctx:YAPLParser.ClassExprContext):
-        global currentClass
-        global currentMethod
-        global currentScope
+
         className = str(ctx.TYPEID()[0])
         if len(ctx.TYPEID()) > 1:
             parentClass = str(ctx.TYPEID()[1])
         else:
             parentClass = None
         entry = ClassTableEntry(className, parentClass)
-        classTable.addEntry(entry)
-        currentClass = className
-        currentMethod = None
-        currentScope = 1
+        self.classTable.addEntry(entry)
+        self.currentClass = className
+        self.currentMethod = None
+        self.currentScope = 1
         return self.visitChildren(ctx)
 
     def visitMethod(self, ctx:YAPLParser.MethodContext): 
-        global currentMethod
-        global currentScope
-        global currentClass
-        global currentMethodId
-        currentMethodId += 1
-        functionName = str(ctx.OBJECTID())
+
+        self.currentMethodId += 1
+
+        functionName = str(ctx.ID())
         type = str(ctx.TYPEID())
-        entry = FunctionTableEntry(currentMethodId,functionName, type, currentScope, currentClass)
-        functionTable.addEntry(entry)
-        currentMethod = functionName
+        entry = FunctionTableEntry(self.currentMethodId,functionName, type, currentScope, self.currentClass)
+        self.functionTable.addEntry(entry)
+        self.currentMethod = functionName
         currentScope = 2
         return self.visitChildren(ctx)
     
     def visitFeature(self, ctx:YAPLParser.FeatureContext):
 
-        global currentMethod
-        global currentScope
-        global currentClass
-        global currentMethodId
-        featureName = str(ctx.OBJECTID())
+  
+        featureName = str(ctx.ID())
         featureType = str(ctx.TYPEID())
-        if currentMethod:
-            entry = AttributeTableEntry(featureName, featureType, currentScope, currentClass, currentMethodId)
+        if self.currentMethod:
+            entry = AttributeTableEntry(featureName, featureType, self.currentScope, self.currentClass, self.currentMethodId)
         else:
-            entry = AttributeTableEntry(featureName, featureType, currentScope, currentClass, None)
-        attributeTable.addEntry(entry)
+            entry = AttributeTableEntry(featureName, featureType, self.currentScope, self.currentClass, None)
+        self.attributeTable.addEntry(entry)
 
         return self.visitChildren(ctx)
 
     def visitFormal(self, ctx:YAPLParser.FormalContext):
-        global currentMethod
-        global currentClass
-        global currentScope
-        featureName = str(ctx.OBJECTID())
+
+        featureName = str(ctx.ID())
         featureType = str(ctx.TYPEID())
-        entry = AttributeTableEntry(featureName, featureType, currentScope, currentClass, currentMethodId)
-        attributeTable.addEntry(entry)
+        entry = AttributeTableEntry(featureName, featureType, self.currentScope, self.currentClass, self.currentMethodId)
+        self.attributeTable.addEntry(entry)
         return self.visitChildren(ctx)
 
     def visitAdd(self, ctx:YAPLParser.AddContext):
-        global foundErrors
+
         childrenResults = []
         for node in ctx.expr():
             childresult = self.visit(node)
@@ -87,7 +80,7 @@ class YaplVisitorEditedBase(YAPLVisitor):
             return "Int"
         else:
             error = semanticError(ctx.start.line, "Cannot add " + childrenResults[0] + " and " + childrenResults[-1])
-            foundErrors.append(error)
+            self.foundErrors.append(error)
             return "Error"
         
     
@@ -115,22 +108,22 @@ class YaplVisitorEditedBase(YAPLVisitor):
             self.foundErrors.append(error)
             return "Error"
 
-   # Visit a parse tree produced by YAPLParser#lessThan.
+    # Visit a parse tree produced by YAPL2Parser#lessExpr.
     def visitLessThan(self, ctx:YAPLParser.LessThanContext):
-        childrenResults = []
-        for node in ctx.expr():
-            childrenResults.append(self.visit(node))
-        if childrenResults[0] == "Int" and childrenResults[1] == "Int":
-            return "Bool"
-        else:
-            error = semanticError(ctx.start.line, "Cannot compare " + childrenResults[0] + " and " + childrenResults[1])
-            self.foundErrors.append(error)
-            return "Error"
+         childrenResults = []
+         for node in ctx.expr():
+             childrenResults.append(self.visit(node))
+         if childrenResults[0] == "Int" and childrenResults[1] == "Int":
+             return "Bool"
+         else:
+             error = semanticError(ctx.start.line, "Cannot compare " + childrenResults[0] + " and " + childrenResults[1])
+             self.foundErrors.append(error)
+             return "Error"
 
 
     # Visit a parse tree produced by YAPLParser#divide.
     def visitDivide(self, ctx:YAPLParser.DivideContext):
-        global foundErrors
+
         childrenResults = []
         for node in ctx.expr():
             childresult = self.visit(node)
@@ -140,25 +133,25 @@ class YaplVisitorEditedBase(YAPLVisitor):
             return "Int"
         else:
             error = semanticError(ctx.start.line, "Cannot divide " + childrenResults[0] + " and " + childrenResults[-1])
-            foundErrors.append(error)
+            self.foundErrors.append(error)
             return "Error"
 
 
     # Visit a parse tree produced by YAPLParser#lessEqual.
     def visitLessEqual(self, ctx:YAPLParser.LessEqualContext):
-        childrenResults = []
-        for node in ctx.expr():
-            childrenResults.append(self.visit(node))
-        if childrenResults[0] == "Int" and childrenResults[1] == "Int":
-            return "Bool"
-        else:
-            error = semanticError(ctx.start.line, "Cannot compare " + childrenResults[0] + " and " + childrenResults[1])
-            self.foundErrors.append(error)
-            return "Error"
+         childrenResults = []
+         for node in ctx.expr():
+             childrenResults.append(self.visit(node))
+         if childrenResults[0] == "Int" and childrenResults[1] == "Int":
+             return "Bool"
+         else:
+             error = semanticError(ctx.start.line, "Cannot compare " + childrenResults[0] + " and " + childrenResults[1])
+             self.foundErrors.append(error)
+             return "Error"
 
     # Visit a parse tree produced by YAPLParser#multiply.
     def visitMultiply(self, ctx:YAPLParser.MultiplyContext):
-         global foundErrors
+
          childrenResults = []
          for node in ctx.expr():
              childresult = self.visit(node)
@@ -168,13 +161,13 @@ class YaplVisitorEditedBase(YAPLVisitor):
              return "Int"
          else:
              error = semanticError(ctx.start.line, "Cannot multiply " + childrenResults[0] + " and " + childrenResults[-1])
-             foundErrors.append(error)
+             self.foundErrors.append(error)
              return "Error"
 
 
     # Visit a parse tree produced by YAPLParser#substract.
     def visitSubstract(self, ctx:YAPLParser.SubstractContext):
-        global foundErrors
+
         childrenResults = []
         for node in ctx.expr():
             childresult = self.visit(node)
@@ -184,7 +177,7 @@ class YaplVisitorEditedBase(YAPLVisitor):
             return "Int"
         else:
             error = semanticError(ctx.start.line, "Cannot subtract " + childrenResults[0] + " and " + childrenResults[-1])
-            foundErrors.append(error)
+            self.foundErrors.append(error)
             return "Error"
 
     def visitInteger(self, ctx:YAPLParser.IntegerContext):
@@ -198,46 +191,50 @@ class YaplVisitorEditedBase(YAPLVisitor):
         return "Bool"
         
     def visitAssignment(self, ctx:YAPLParser.AssignmentContext):
-         global currentMethod
-         global currentClass
-         global currentScope
-         global currentMethodId
-         global foundErrors
-         leftside = str(ctx.OBJECTID())
+
+         leftside = str(ctx.ID())
          #search for leftside in attribute table
-         leftsideEntry = attributeTable.findEntry(leftside, currentClass, currentMethodId)
+         leftsideEntry = self.attributeTable.findEntry(leftside, self.currentClass, self.currentMethodId)
          #if not found, search without methdod
          if leftsideEntry is None:
-             leftsideEntry = attributeTable.findEntry(leftside, currentClass, None)
+             leftsideEntry = self.attributeTable.findEntry(leftside, self.currentClass, None)
          if leftsideEntry is None:
              error = semanticError(ctx.start.line, "Variable " + leftside + " not defined")
-             foundErrors.append(error)
+             self.foundErrors.append(error)
              return "Error"
          childrenResult = self.visitChildren(ctx)
          if childrenResult == leftsideEntry.type:
              return leftsideEntry.type
          else:
              error = semanticError(ctx.start.line, "Can't assign " + childrenResult + " to " + leftsideEntry.type)
-             foundErrors.append(error)
+             self.foundErrors.append(error)
              return "Error"
 
     def visitId(self, ctx:YAPLParser.IdContext):
-         global currentMethod
-         global currentClass
-         global currentScope
-         global currentMethodId
-         global foundErrors
-         varName = str(ctx.OBJECTID())
+
+         varName = str(ctx.ID())
          if varName == "self":
              return "SELF_TYPE"
          #search for varName in attribute table
          else:
-             varEntry = attributeTable.findEntry(varName, currentClass, currentMethodId)  
+             varEntry = self.attributeTable.findEntry(varName, self.currentClass, self.currentMethodId)  
              if varEntry is None:
-                 varEntry = attributeTable.findEntry(varName, currentClass, None)
+                 varEntry = self.attributeTable.findEntry(varName, self.currentClass, None)
              if varEntry is None:
                  error = semanticError(ctx.start.line, "Variable " + varName + " not defined")
-                 foundErrors.append(error)
+                 self.foundErrors.append(error)
                  return "Error"
              else:
                  return varEntry.type
+
+    def visitString(self, ctx:YAPLParser.StringExprContext):
+         return "String"
+    
+    def visitNot(self, ctx:YAPLParser.NotContext):
+        childrenResult = self.visit(ctx.expr())
+        if childrenResult == "Bool":
+            return "Bool"
+        else:
+            error = semanticError(ctx.start.line, "Cannot use NOT operator on  " + childrenResult)
+            self.foundErrors.append(error)
+            return "Error"
