2b4c19e814b3ec67815384011cf105609c51c88c
sebastiangonzalez <dsgspersonal@gmail.com>
2023-10-02 19:24:49 -0600

    dark mode

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import os
-from tkinter.ttk import Style
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
 from antlr4 import *
 from antlr4.tree.Trees import Trees
-
 from YAPL2Lexer import YAPL2Lexer
 from YAPL2Parser import YAPL2Parser
 from YAPL2Visitor import YAPL2Visitor
 from first_visitor import FirstVisitor
 from intermediate_code_generator import IntermediateCodeGenerator
 from errors import semanticError
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-
 
 def tablePrint(visitor):
     print("==============================SYMBOL TABLE==============================")
@@ -31,7 +27,6 @@ def tablePrint(visitor):
         print(i)
     print("==============================END==============================")
 
-
 def set_dark_mode(widget):
     widget.configure(bg="#2E2E2E", fg="white")
 
@@ -40,15 +35,13 @@ def gui():
     window.configure(bg="#2E2E2E")  # Set the main window background color
     buttonsFrame = tk.Frame(window, bg="#2E2E2E")
     codeAndIC = tk.Frame(window, bg="#2E2E2E")
-    buttonsFrame.pack()
-    codeAndIC.pack(fill=tk.X)
+    buttonsFrame.pack(side=tk.LEFT)  # Move buttons to the left side
+    codeAndIC.pack(fill=tk.BOTH, expand=True)
     window.title("YAPL2 Code")
 
     user_input = tk.Text(codeAndIC)
-
     set_dark_mode(user_input)
     intermediateCode = tk.Text(codeAndIC)
-
     set_dark_mode(intermediateCode)
 
     errorsWindow = tk.Text(width=100)
@@ -68,17 +61,17 @@ def gui():
     compileButton = ttk.Button(buttonsFrame, text="Compile", command=lambda: main(user_input.get("1.0", tk.END), errorsWindow, intermediateCode), style='Dark.TButton')
     loadButton = ttk.Button(buttonsFrame, text="Load File", command=lambda: loadFile(user_input), style='Dark.TButton')
 
-    compileButton.pack(side=tk.LEFT)
-    loadButton.pack(side=tk.LEFT)
+    compileButton.pack(side=tk.LEFT, fill=tk.X)
+    loadButton.pack(side=tk.LEFT, fill=tk.BOTH)
     user_input.pack(side=tk.LEFT, fill=tk.Y)
-    intermediateCode.pack(side=tk.RIGHT, 
-                          fill=tk.Y)
-    errorsWindow.pack(fill=tk.X)
+    intermediateCode.pack(side=tk.RIGHT, fill=tk.Y)
+    errorsWindow.pack(fill=tk.BOTH, expand=True)
 
     window.mainloop()
+
 def loadFile(userInputWindow):
     userInputWindow.delete("1.0", tk.END)
-    filename = fd.askopenfilename(initialdir = os.getcwd(), title = "Select file")
+    filename = fd.askopenfilename(initialdir=os.getcwd(), title="Select file")
     with open(filename, 'r') as f:
         lines = f.read()
         userInputWindow.insert(tk.END, lines)
@@ -100,7 +93,6 @@ def main(program, errorsWindow, interMediateCodeWindow):
 
     #tablePrint(firstVisitor)
     
-
     visitor = YAPL2Visitor(firstVisitor.classTable, firstVisitor.functionTable, firstVisitor.attributeTable, firstVisitor.typesTable, firstVisitor.foundErrors)
     visitor.visit(tree)
     # Showing tables
@@ -117,17 +109,15 @@ def main(program, errorsWindow, interMediateCodeWindow):
 
     if len(visitor.foundErrors) > 0:
         for i in visitor.foundErrors:
-            errorsWindow.insert(tk.END,str(i)+"\n")
-            interMediateCodeWindow.insert(tk.END,"Trash in trash out, fix errors")
+            errorsWindow.insert(tk.END, str(i) + "\n")
+            interMediateCodeWindow.insert(tk.END, "Trash in trash out, fix errors")
             return -1
     else:
         errorsWindow.insert(tk.END, "No errors :D\n")
 
-    
     intCodeGenerator = IntermediateCodeGenerator(visitor.classTable, visitor.functionTable, visitor.attributeTable, visitor.typesTable)
     intermediateCode = intCodeGenerator.visit(tree)
-    interMediateCodeWindow.insert(tk.END,str(intermediateCode))
-    
+    interMediateCodeWindow.insert(tk.END, str(intermediateCode))
+
 if __name__ == "__main__":
     gui()
-    
